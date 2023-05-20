817327b277e84378c85939081b80b7bf669dc08a
Moksh Mahajan <mokshmahajan008@gmail.com>
2023-05-20 21:16:46 +0530

    setup react-router for navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { FiltersProvider } from "./contexts/FiltersContext";
 import Cart from "./pages/Cart";
 import Products from "./pages/Products";
-import Mockman from "mockman-js";
+import WishList from "./pages/WishList";
 
 function App() {
   return (
-    <FiltersProvider>
-      {/* <Cart /> */}
-      <Products />
-    </FiltersProvider>
+    <div>
+      <div>
+        <Link to="/wishlist">Wishlist</Link> || <Link to="/cart">Cart</Link>
+      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <FiltersProvider>
+              <Products />
+            </FiltersProvider>
+          }
+        />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/wishlist" element={<WishList />} />
+      </Routes>
+    </div>
   );
 }
 
