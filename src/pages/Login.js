c2cb39f8778ae417d9b02d658a0eef0eb6ac022d
Moksh Mahajan <mokshmahajan008@gmail.com>
2023-06-04 13:35:54 +0530

    add signup and login forms

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,54 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 
 export function Login() {
   const { handleLogin } = useContext(AuthContext);
+  const [authMode, setAuthMode] = useState("login");
+
+  const toggleAuthMode = () =>
+    setAuthMode((authMode) => (authMode == "login" ? "signup" : "login"));
+
+  return authMode === "login" ? (
+    <LoginForm onSignUp={toggleAuthMode} />
+  ) : (
+    <SignUpForm onLogin={toggleAuthMode} />
+  );
+}
+
+function SignUpForm({ onLogin }) {
   return (
     <div>
-      <button onClick={handleLogin}>Login</button>
+      <h1>Sign Up</h1>
+      <div>
+        <input placeholder="First Name"></input>
+      </div>
+      <div>
+        <input placeholder="Last Name"></input>
+      </div>
+      <div>
+        <input placeholder="Email"></input>
+      </div>
+      <div>
+        <input placeholder="Password"></input>
+      </div>
+      <button>Create Account</button>
+      <button onClick={onLogin}>Already have an account? Login</button>
+    </div>
+  );
+}
+
+function LoginForm({ onSignUp }) {
+  return (
+    <div>
+      <h1>Log in</h1>
+      <div>
+        <input placeholder="Email"></input>
+      </div>
+      <div>
+        <input placeholder="Password"></input>
+      </div>
+      <button>Login</button>
+      <button onClick={onSignUp}>Don't have an account? Signup</button>
     </div>
   );
 }
